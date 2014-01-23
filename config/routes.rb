44a267f470855b20a272519f8fe1480ad4ab1c76
Allyson Barros <allysonbarrosrn@gmail.com>
2014-01-23 11:07:30 -0300

    [6-5-stable] trazendo o sistema de notificações para a versão 6.5 do GitLab.

--- config/routes.rb
+++ config/routes.rb
@@ -134,7 +134,14 @@ Gitlab::Application.routes.draw do
 
   match "/u/:username" => "users#show", as: :user, constraints: { username: /.*/ }, via: :get
 
-
+  #
+  # Event Notifications Area
+  #
+  match "/account/notifications/inbox" => "event_notifications#inbox", as: :inbox_user_notifications, via: :get
+  match "/account/notifications/sent" => "event_notifications#sent", as: :sent_user_notifications, via: :get
+  match "/account/notifications/:id/read/" => "event_notifications#show", as: :show_user_notification, via: :get
+  match "/account/notifications/:id/read/mark-as-unread" => "event_notifications#mark_as_unread", as: :user_notification_mark_as_unread, via: :get
+  match "/account/notifications/:id/delete" => "event_notifications#destroy", as: :destroy_user_notification, via: :delete
 
   #
   # Dashboard Area
