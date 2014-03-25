c53f5a204a8f917a35a7a27712794e8acba9b401
Allyson Barros <allyson.barros@ifrn.edu.br>
2014-03-25 11:54:23 -0300

    Ajuste na tela de listagem de merge-request.

--- app/models/concerns/issuable.rb
+++ app/models/concerns/issuable.rb
@@ -93,6 +93,11 @@ module Issuable
     end
   end
 
+  def downvoters
+    downvotes = notes.select(&:downvote?)
+    downvotes.map(&:author)
+  end
+
   # Return the number of +1 comments (upvotes)
   def upvotes
     notes.select(&:upvote?).size
@@ -104,6 +109,11 @@ module Issuable
     else
       100.0 / votes_count * upvotes
     end
+  end
+
+  def upvoters
+    upvotes = notes.select(&:upvote?)
+    upvotes.map(&:author)
   end
 
   # Return the total number of votes
