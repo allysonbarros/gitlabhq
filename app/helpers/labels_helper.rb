bbaacefa2cfe4d93bdafcb43c41114b3a91b66b8
Allyson Barros <allyson.barros@ifrn.edu.br>
2014-04-24 10:58:22 -0300

    Implementada a tafera #181.

--- app/helpers/labels_helper.rb
+++ app/helpers/labels_helper.rb
@@ -1,28 +1,36 @@
 module LabelsHelper
-  def issue_label_names
-    @project.issues_labels.map(&:name)
-  end
-
-  def labels_autocomplete_source
-    labels = @project.issues_labels
-    labels = labels.map{ |l| { label: l.name, value: l.name } }
-    labels.to_json
-  end
-
-  def label_css_class(name)
-    klass = Gitlab::IssuesLabels
-
-    case name.downcase
-    when *klass.warning_labels
-      'label-warning'
-    when *klass.neutral_labels
-      'label-primary'
-    when *klass.positive_labels
-      'label-success'
-    when *klass.important_labels
-      'label-danger'
-    else
-      'label-info'
+    def issue_label_names
+        @project.issues_labels.map(&:name)
     end
-  end
+
+    def labels_autocomplete_source
+        labels = @project.issues_labels
+        labels = labels.map{ |l| { label: l.name, value: l.name } }
+        labels.to_json
+    end
+
+    def label_css_class(name)
+        klass = Gitlab::IssuesLabels
+
+        case name.downcase
+        when *klass.warning_labels
+            'label-warning'
+        when *klass.neutral_labels
+            'label-primary'
+        when *klass.positive_labels
+            'label-success'
+        when *klass.important_labels
+            'label-danger'
+        when *klass.info_labels
+            'label-info'
+        when *klass.adm_labels
+            'label-info'
+        when *klass.rh_labels
+            'label-warning'
+        when *klass.edu_labels
+            'label-success'
+        else
+            'label-default'
+        end
+    end
 end
--- lib/gitlab/issues_labels.rb
+++ lib/gitlab/issues_labels.rb
@@ -1,28 +1,48 @@
 module Gitlab
-  class IssuesLabels
-    class << self
-      def important_labels
-        %w(bug critical confirmed)
-      end
+    class IssuesLabels
+        class << self
+            def important_labels
+                %w(bug critical confirmed)
+            end
 
-      def warning_labels
-        %w(documentation support)
-      end
+            def warning_labels
+                %w(documentation support)
+            end
 
-      def neutral_labels
-        %w(discussion suggestion)
-      end
+            def neutral_labels
+                %w(comum)
+            end
 
-      def positive_labels
-        %w(feature enhancement)
-      end
+            def positive_labels
+                %w(feature enhancement)
+            end
 
-      def generate(project)
-        labels = important_labels + warning_labels + neutral_labels + positive_labels
+            def info_labels
+                %w(discussion suggestion)
+            end
 
-        project.issues_default_label_list = labels
-        project.save
-      end
+            def default_labels
+                %w(demanda)
+            end
+
+            def adm_labels
+                %w(adm ae almoxarifado chaves clipping compras contratos extensao frota patrimonio pdi planejamento projetos protocolo saude)
+            end
+
+            def rh_labels
+                %w(rh arquivo ferias ponto remanejamento)
+            end
+
+            def edu_labels
+                %w(edu)
+            end
+
+            def generate(project)
+                labels = important_labels + warning_labels + neutral_labels + positive_labels + info_labels + default_labels + adm_labels + rh_labels + edu_labels
+
+                project.issues_default_label_list = labels
+                project.save
+            end
+        end
     end
-  end
 end
