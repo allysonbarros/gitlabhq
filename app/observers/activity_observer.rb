ec61e0f899083b3bb6f7a911c0340dcae941a67c
Allyson Barros <allyson.barros@ifrn.edu.br>
2014-06-11 10:30:23 -0300

    Voltando para a versão 6-8-stable e habilitando as notificações.

--- app/observers/activity_observer.rb
+++ app/observers/activity_observer.rb
@@ -1,47 +1,45 @@
-# class ActivityObserver < BaseObserver
-#   observe :issue, :note, :milestone
+class ActivityObserver < BaseObserver
+  observe :issue, :note, :milestone
 
-#   def after_create(record)
-#     event_author_id = record.author_id
+  def after_create(record)
+    event_author_id = record.author_id
 
-#     if record.kind_of?(Note)
-#       # Skip system notes, like status changes and cross-references.
-#       return true if record.system?
+    if record.kind_of?(Note)
+      # Skip system notes, like status changes and cross-references.
+      return true if record.system?
 
-#       # Skip wall notes to prevent spamming of dashboard
-#       return true if record.noteable_type.blank?
-#     end
+      # Skip wall notes to prevent spamming of dashboard
+      return true if record.noteable_type.blank?
+    end
 
-#     if event_author_id
-#       event = create_event(record, Event.determine_action(record))
+    if event_author_id
+      event = create_event(record, Event.determine_action(record))
       
-#       if record.kind_of?(Note) and Event.determine_action(record) == Event::COMMENTED
-#         create_event_notification(record, event)
-#       end
-#     end
-#   end
+      if record.kind_of?(Note) and Event.determine_action(record) == Event::COMMENTED
+        create_event_notification(record, event)
+      end
+    end
+  end
 
-#   def after_close(record, transition)
-#     create_event(record, Event::CLOSED)
-#   end
+  def after_close(record, transition)
+    create_event(record, Event::CLOSED)
+  end
 
-#   def after_reopen(record, transition)
-#     create_event(record, Event::REOPENED)
-#   end
+  def after_reopen(record, transition)
+    create_event(record, Event::REOPENED)
+  end
 
-#   protected
+  protected
 
-#   def create_event(record, status)
-#     event = Event.create(
-#       project: record.project,
-#       target_id: record.id,
-#       target_type: record.class.name,
-#       action: status,
-#       author_id: current_user.id
-#     )
+  def create_event(record, status)
+    event = Event.create(
+      project: record.project,
+      target_id: record.id,
+      target_type: record.class.name,
+      action: status,
+      author_id: current_user.id
+    )
 
-#     return event
-#   end
-
-  
-# end
+    return event
+  end
+end
